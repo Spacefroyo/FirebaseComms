db41926d6a2ae5fc5a11ab282d55eec9d857d93e
Spacefroyo <petergao322@gmail.com>
2022-06-06 09:37:07 -0700

    Initial Commit

---
+++ FirebaseComms/FirebaseCommsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseCommsApp.swift
+//  FirebaseComms
+//
+//  Created by Peter Gao on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseCommsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
